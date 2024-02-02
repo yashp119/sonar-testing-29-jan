baf2969abd7d1021fa8a628162ec0b93c1dc312e
yashp119 <125194772+yashp119@users.noreply.github.com>
2024-02-02 19:07:11 +0530

    Update css.php

--- css.php
+++ css.php
@@ -12,7 +12,7 @@
 		echo "<h2>PHP is Fun!</h2>";
 		echo "Hello world!<br>";
 		echo "I'm about to learn PHP!<br>";
-		echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+		echo "This ", "string ", "was ", "made ", "with multiplhe parameters.";
 	?>
 
 	<?php
